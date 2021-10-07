b9a2599db1bc9b477bac0c871620bbc6a2c37f66
namyul <bny64@naver.com>
2021-10-07 17:13:15 +0900

    nestjs/controller에 대해 공부

--- project/src/app.controller.ts
+++ project/src/app.controller.ts
@@ -1,14 +1,22 @@
 // * 하나의 라우트가 있는 기본 컨트롤러
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Req } from '@nestjs/common';
+import { Request } from 'express';
 import { AppService } from './app.service';
 
-@Controller()
+//@와 같은 데코레이터는 기능을 첨가한다고 생각하면 된다.
+@Controller('cats')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
+  @Get('hello/:id')
+  getHello(
+    @Req() req: Request,
+    @Body() Body,
+    @Param() param: { id: string },
+  ): string {
     //service의 return값을 받고 return값이 모듈로 들어가게 되고 모듈이 nestFactory에 들어가게 되서 자동으로 client로 return
+    //console.log(req);
+    //console.log(param);
     return this.appService.getHello();
   }
 }
