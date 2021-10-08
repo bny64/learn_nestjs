66bad66e5056c546c6a78533fc22ff69ab165b9b
namyul <bny64@naver.com>
2021-10-08 17:57:59 +0900

    nestjs + nodejs 25강 완료(exception, filter 등등)

--- project/src/cats/cats.controller.ts
+++ project/src/cats/cats.controller.ts
@@ -1,17 +1,33 @@
-import { Controller, Delete, Get, Patch, Post, Put } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  HttpException,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Put,
+  UseFilters,
+} from '@nestjs/common';
+import { HttpExceptionFilter } from 'src/http-exception.filter';
 import { CatsService } from './cats.service';
 
 @Controller('cats')
+@UseFilters(HttpExceptionFilter) //class 전체에서도 UseFilters 적용 가능하다.
 export class CatsController {
   constructor(private readonly catsServce: CatsService) {}
 
   @Get()
+  //@UseFilters(HttpExceptionFilter) //해당 서비스에서 발생한 exception은 useFilter에서 적용한 filter로 넘어간다.
   getAllCat() {
-    return 'all cat';
+    throw new HttpException({ power: 'api is broken!', mode: 404 }, 401);
+    return 'get all cat api';
   }
 
   @Get(':id')
-  getOneCat() {
+  getOneCat(@Param('id', ParseIntPipe) param: number) {
+    console.log(param);
     return 'one cat';
   }
 
--- project/src/main.ts
+++ project/src/main.ts
@@ -1,9 +1,11 @@
+import { HttpExceptionFilter } from 'src/http-exception.filter';
 // 핵심기능 NestFactory를 사용하여 Nest 애플리케이션 인스턴스를 생성하는 애플리케이션의 엔트리 파일입니다.
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(8000);
 }
 bootstrap();
