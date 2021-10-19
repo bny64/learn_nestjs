43f68fda94a3dd3878bd4878378a9780979dc734
namyul <bny64@naver.com>
2021-10-19 17:06:53 +0900

    nestjs 강의 : DB연결 완료

--- chattings/src/app.module.ts
+++ chattings/src/app.module.ts
@@ -1,10 +1,15 @@
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, //이 옵션을 사용하면 다른 곳에서 모듈을 가져올 필요없이 global로 사용할 수 있다.
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+  ],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
