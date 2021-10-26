b45362b12b2719c37f09a2b41d7837041318baf0
namyul <bny64@naver.com>
2021-10-26 14:32:51 +0900

    test

--- practice_project/dist/cats/cats.service.d.ts
+++ practice_project/dist/cats/cats.service.d.ts
@@ -1,6 +1,5 @@
-import { Request, Response } from "express";
-export declare const readAllcat: (req: Request, res: Response) => void;
-export declare const readCat: (req: Request, res: Response) => void;
-export declare const createCat: (req: Request, res: Response) => void;
-export declare const updateCat: (req: Request, res: Response) => void;
-export declare const deleteCat: (req: Request, res: Response) => void;
+export declare const readAllcat: (req: any, res: any) => void;
+export declare const readCat: (req: any, res: any) => void;
+export declare const createCat: (req: any, res: any) => void;
+export declare const updateCat: (req: any, res: any) => void;
+export declare const deleteCat: (req: any, res: any) => void;
